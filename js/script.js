9fb6e9253eab30edb14630a5267cb62dc5f85cb6
hisa <76601471+hisaaashi@users.noreply.github.com>
2022-10-29 19:29:17 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ $(function() {
       var header_height = $("header").height();
       var href= $(this).attr("href");
       var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top - header_height;
+      var position = target.offset().top;
       $('body,html').animate({scrollTop:position}, speed, 'swing');
       return false;
     });
