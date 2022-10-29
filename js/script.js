5191658419ad22fb15c55dcb6c0cf2fa3e03d14a
hisa <76601471+hisaaashi@users.noreply.github.com>
2022-10-29 13:40:37 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,16 +35,13 @@ $(function() {
 
 
   // #から始まるURLがクリックされた時
-  jQuery('a[href^="#"]').click(function() {
-    // .headerクラスがついた要素の高さを取得
-    let header = jQuery(".header").innerHeight();
-    let speed = 300;
-    let id = jQuery(this).attr("href");
-    let target = jQuery("#" == id ? "html" : id);
-    // トップからの距離からヘッダー分の高さを引く
-    let position = jQuery(target).offset().top - header;
-    // その分だけ移動すればヘッダーと被りません
-    if ("fixed" !== jQuery(".header").css("position")) {
+	jQuery('a[href^="#"]').click(function() {
+		let header = jQuery(".header").innerHeight();
+		let speed = 300;
+		let id = jQuery(this).attr("href");
+		let target = jQuery("#" == id ? "html" : id);
+		let position = jQuery(target).offset().top - header;
+		if ("fixed" !== jQuery(".header").css("position")) {
 			position = jQuery(target).offset().top;
 		}
 		if (0 > position) {
@@ -57,7 +54,8 @@ $(function() {
 			speed
 		);
 		return false;
-  });
+	});
+
 
 
   // スクロール検知
