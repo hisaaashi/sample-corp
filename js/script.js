7f7803c2cd1105f2d8a80aed14d293453d835869
hisa <76601471+hisaaashi@users.noreply.github.com>
2022-10-29 16:36:59 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,28 +34,27 @@ $(function() {
   });
 
 
-	/* スムーススクロール */
-	jQuery('a[href^="#"]').click(function() {
-		let header = jQuery(".js-header").innerHeight();
-		let speed = 300;
-		let id = jQuery(this).attr("href");
-		let target = jQuery("#" == id ? "html" : id);
-		let position = jQuery(target).offset().top - header;
-		if ("fixed" !== jQuery(".js-header").css("position")) {
-			position = jQuery(target).offset().top;
-		}
-		if (0 > position) {
-			position = 0;
-		}
-		jQuery("html, body").animate(
-			{
-				scrollTop: position
-			},
-			speed
-		);
-		return false;
-	});
-
+// スムーススクロール
+jQuery('a[href^="#"]').click(function () {
+  let header = jQuery(".js-header").innerHeight();
+  let speed = 300;
+  let id = jQuery(this).attr("href");
+  let target = jQuery("#" == id ? "html" : id);
+  let position = jQuery(target).offset().top - header;
+  if ("fixed" !== jQuery(".js-header").css("position")) {
+    position = jQuery(target).offset().top;
+  }
+  if (0 > position) {
+    position = 0;
+  }
+  jQuery("html, body").animate(
+    {
+      scrollTop: position,
+    },
+    speed
+  );
+  return false;
+});
 
   // スクロール検知
   jQuery(window).on("scroll", function() {
