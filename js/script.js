30cb76bcc17017c0735b181a30765432d667b4f5
hisa <76601471+hisaaashi@users.noreply.github.com>
2022-10-29 16:06:12 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,27 +34,27 @@ $(function() {
   });
 
 
-  // #から始まるURLがクリックされた時
-jQuery('a[href^="#"]').click(function () {
-  let header = jQuery(".header").innerHeight();
-  let speed = 300;
-  let id = jQuery(this).attr("href");
-  let target = jQuery("#" == id ? "html" : id);
-  let position = jQuery(target).offset().top - header;
-  if ("fixed" !== jQuery(".header").css("position")) {
-    position = jQuery(target).offset().top;
-  }
-  if (0 > position) {
-    position = 0;
-  }
-  jQuery("html, body").animate(
-    {
-      scrollTop: position,
-    },
-    speed
-  );
-  return false;
-});
+	/* スムーススクロール */
+	jQuery('a[href^="#"]').click(function() {
+		let header = jQuery(".header").innerHeight();
+		let speed = 300;
+		let id = jQuery(this).attr("href");
+		let target = jQuery("#" == id ? "html" : id);
+		let position = jQuery(target).offset().top - header;
+		if ("fixed" !== jQuery(".header").css("position")) {
+			position = jQuery(target).offset().top;
+		}
+		if (0 > position) {
+			position = 0;
+		}
+		jQuery("html, body").animate(
+			{
+				scrollTop: position
+			},
+			speed
+		);
+		return false;
+	});
 
 
   // スクロール検知
