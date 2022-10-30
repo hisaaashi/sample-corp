4a3903452bba4e48b11528ed678e2a5a76e63b7f
hisa <76601471+hisaaashi@users.noreply.github.com>
2022-10-30 13:05:25 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -34,38 +34,25 @@ $(function() {
   });
 
 
-   // #から始まるURLがクリックされた時
-//   jQuery('a[href^="#"]').click(function() {
-//     // .headerクラスがついた要素の高さを取得
-//     let header = jQuery(".header").innerHeight();
-//     let speed = 300;
-//     let id = jQuery(this).attr("href");
-//     let target = jQuery("#" == id ? "html" : id);
-//     // トップからの距離からヘッダー分の高さを引く
-//     let position = jQuery(target).offset().top - header;
-//     // その分だけ移動すればヘッダーと被りません
-//     jQuery("html, body").animate(
-//       {
-//         scrollTop: position
-//       },
-//       speed
-//     );
-//     return false;
-//   });
-  
-
-  // 変数pagetopの宣言
-  var pagetop = $('.to-top');
-
-  // ページトップへ戻るボタンをクリックしたとき
-  pagetop.on('click', function() {
-    $('html, body').animate (
-      { scrollTop : 0 },
-      400,
-      'swing',
+  // #から始まるURLがクリックされた時
+  jQuery('a[href^="#"]').click(function() {
+    // .headerクラスがついた要素の高さを取得
+    let header = jQuery(".header").innerHeight();
+    let speed = 300;
+    let id = jQuery(this).attr("href");
+    let target = jQuery("#" == id ? "html" : id);
+    // トップからの距離からヘッダー分の高さを引く
+    let position = jQuery(target).offset().top - header;
+    // その分だけ移動すればヘッダーと被りません
+    jQuery("html, body").animate(
+      {
+        scrollTop: position
+      },
+      speed
     );
     return false;
   });
+
 
   // スクロール検知
   jQuery(window).on("scroll", function() {
