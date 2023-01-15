624de4ba6aa4b59222cb3d8f9a128e6a04a0aa16
hisa <76601471+hisaaashi@users.noreply.github.com>
2023-01-15 16:24:04 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -35,26 +35,26 @@ $(function() {
 
 
   // #から始まるURLがクリックされた時
-  // jQuery('a[href^="#"]').click(function() {
-  //   // .headerクラスがついた要素の高さを取得
-  //   let header = jQuery(".header").innerHeight();
-  //   let speed = 300;
-  //   let id = jQuery(this).attr("href");
-  //   let target = jQuery("#" == id ? "html" : id);
-  //   // トップからの距離からヘッダー分の高さを引く
-  //   let position = jQuery(target).offset().top - header;
-  //   // その分だけ移動すればヘッダーと被りません
-  //   jQuery("html, body").animate(
-  //     {
-  //       scrollTop: position
-  //     },
-  //     speed
-  //   );
-  //   return false;
-  // });
+  jQuery('a[href^="#"]').click(function() {
+    // .headerクラスがついた要素の高さを取得
+    let header = jQuery(".header").innerHeight();
+    let speed = 300;
+    let id = jQuery(this).attr("href");
+    let target = jQuery("#" == id ? "html" : id);
+    // トップからの距離からヘッダー分の高さを引く
+    let position = jQuery(target).offset().top - header;
+    // その分だけ移動すればヘッダーと被りません
+    jQuery("html, body").animate(
+      {
+        scrollTop: position
+      },
+      speed
+    );
+    return false;
+  });
 
   // #page-topをクリックした際の設定
-$('#page-top').click(function () {
+$('#top').click(function () {
   $('body,html').animate({
       scrollTop: 0//ページトップまでスクロール
   }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
