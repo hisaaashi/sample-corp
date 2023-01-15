5d9e2a03a8fef7aaabf6e767c6d7d7c504315b06
hisa <76601471+hisaaashi@users.noreply.github.com>
2023-01-15 10:55:40 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ $(function() {
     let header = jQuery(".header").innerHeight();
     let speed = 300;
     let id = jQuery(this).attr("href");
-    let target = jQuery("#" == id ? "html" : id);
+    let target = jQuery("#top" == id ? "html" : id);
     // トップからの距離からヘッダー分の高さを引く
     let position = jQuery(target).offset().top - header;
     // その分だけ移動すればヘッダーと被りません
