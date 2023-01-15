de02e535e6e1e3bc01fb33cdae92e4d3a7f5edfe
hisa <76601471+hisaaashi@users.noreply.github.com>
2023-01-15 17:10:52 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -53,15 +53,6 @@ $(function() {
     return false;
   });
 
-  // #page-topをクリックした際の設定
-$('#top').click(function () {
-  $('body,html').animate({
-      scrollTop: 0//ページトップまでスクロール
-  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
-  return false;//リンク自体の無効化
-});
-
-
   // スクロール検知
   jQuery(window).on("scroll", function() {
     // トップから100px以上スクロールしたら
